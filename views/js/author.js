22b4515c835c7ab70746164df03ac71f5cd8caac
Ilyha <mondaxfeall1@gmail.com>
2024-07-02 02:07:49 +0300

    full create form and word generator

--- views/js/author.js
+++ views/js/author.js
@@ -38,27 +38,27 @@ function addAuthor() {
         <div style="display: flex; margin-top: 20px">
             <div style="width: 47%">
                 <label for="authorNumber">Табельный номер:</label>
-                <input type="number" id="author-number" name="authorNumber[]" min="0" required><br><br>
+                <input type="number" id="author-number" name="authorNumbers[]" min="0" required><br><br>
 
                 <label for="authorFIO" style="margin-top: 22px">Фамилия, имя, отчество:</label>
-                <input type="text" id="author-fio" name="authorFIO[]" required><br><br>
+                <input type="text" id="author-fio" name="authorFIOs[]" required><br><br>
 
                 <label for="author-year-birh">Год рождения:</label>
-                <input type="number" id="author-year-birh" name="authorYearBirh[]" min="1900" max="${new Date().getFullYear()}" step="1"  required><br><br>
+                <input type="number" id="author-year-birh" name="authorYearsBirh[]" min="1900" max="${new Date().getFullYear()}" step="1"  required><br><br>
 
                 <label for="authorWorkplace">Место работы:</label>
-                <input type="text" id="author-workplace" name="authorWorkplace" required><br><br>
+                <input type="text" id="author-workplace" name="authorWorkplaces[]" required><br><br>
             </div>
 
             <div style="width: 50%; margin-left: 3%">
                 <label for="author-work-position">Должность:</label>
-                <input type="text" id="author-work-position" name="authorWorkPosition[]" required><br><br>
+                <input type="text" id="author-work-position" name="authorWorkPositions[]" required><br><br>
 
                 <label for="contribution">Обоснование конкретного творческого вклада:</label>
-                <input type="text" id="contribution" name="contribution[]" required><br><br>
+                <input type="text" id="contribution" name="contributions[]" required><br><br>
 
                 <label for="percent-contribution">% авторского вклада:</label>
-                <input type="number" id="percent-contribution" name="percentContribution[]" min="0" max="100" step="1" required><br><br>
+                <input type="number" id="percent-contribution" name="percentageContributions[]" min="0" max="100" step="1" required><br><br>
             </div>
         </div>
     </div>
